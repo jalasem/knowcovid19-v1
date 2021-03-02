ad5397136caa31d4cfd751b13babc3fa71718d3b
Raheem Muiz Olalekan <20601466+Meticulous001@users.noreply.github.com>
2021-03-03 05:01:57 +0800

    Update coronaStats.js

--- assets/data/coronaStats.js
+++ assets/data/coronaStats.js
@@ -2,20 +2,20 @@ export default {
   majorStats: {
     total: {
       heading: 'Total Cases',
-      quantity: 155076,
+      quantity: 156017,
       info: '1st case: 27/02/2020'
     },
     hospitalized: {
       heading: 'Currently Hospitalized',
-      quantity: 20608
+      quantity: 19613
     },
     deaths: {
       heading: 'Deaths',
-      quantity: 1902
+      quantity: 1915
     },
     discharged: {
       heading: 'Discharged',
-      quantity: 132566
+      quantity: 134489
     }
   },
   statesData: [
